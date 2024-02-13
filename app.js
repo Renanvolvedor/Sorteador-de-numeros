3af28d01ee4d611ea029c6a62e62af9ebe5a0b2a
Renan Cardoso Segretti <renanony@gmail.com>
2024-02-13 09:48:47 -0300

    Testando botão Sortear

---
+++ app.js
@@ -0,0 +1,7 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`Quantidade: ${quantidade}. Do número: ${de} até o número: ${ate}.`);
+};
